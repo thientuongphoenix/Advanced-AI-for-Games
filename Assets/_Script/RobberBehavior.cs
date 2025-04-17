584d57760f70c56e4f7742f736dd94dbc1761c3f
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-04-17 15:18:50 +0700

    Implement Method PrintTree

--- Assets/_Script/RobberBehavior.cs
+++ Assets/_Script/RobberBehavior.cs
@@ -8,13 +8,15 @@ public class RobberBehavior : MonoBehaviour
     void Start()
     {
         this.tree = new BehaviorTree(); // root Node
-        Node steal = new Node("Steal Something");
+        Node steal = new Node("Steal Something");  
         Node goToDiamond = new Node("Go To Diamond");
         Node goToVan = new Node("Go To Van");
 
         steal.AddChild(goToDiamond);
         steal.AddChild(goToVan);
         this.tree.AddChild(steal); //root Node -> steal something -> ( 1. Go to diamond; 2. Go to van)
+
+        tree .PrintTree();
     }
 
     // Update is called once per frame
